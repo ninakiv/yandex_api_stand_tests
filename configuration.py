793a1a552cbffc3a6a6c72646620aa891f96d07b
ninakiv <nina4ka999@mail.ru>
2023-10-15 10:40:54 +0400

    Мой первый коммит

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://07a4c3dc-fc26-4432-bbf5-d0cbfebf1ef7.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
